b4b3ede1efb0ce1649b0a1ee70a32a87734aecdf
ПКНЗ <@DESKTOP-OGI312N>
2024-12-10 16:18:32 +0300

    Задание 4.1.17 Поигрался с цветами

--- module4/Program.cs
+++ module4/Program.cs
@@ -7,30 +7,41 @@ class MainClass
     public static void Main(string[] args)
     {
         Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+        Console.WriteLine("cписок доступных цветов:\n blue, \n magenta, \n yellow.");
+        Console.WriteLine();
 
         var color = Console.ReadLine();
 
-      if (color == "red")
+      if (color == "blue")
 {
-    Console.BackgroundColor = ConsoleColor.Red;
-    Console.ForegroundColor = ConsoleColor.Black;
+    Console.BackgroundColor = ConsoleColor.White;
+    Console.ForegroundColor = ConsoleColor.Blue;
 
-    Console.WriteLine("Your color is red!");
+    Console.WriteLine("Your color is blue (on the white background)!");
 }
 
-      else if (color == "green")
+      else if (color == "magenta")
 {
-    Console.BackgroundColor = ConsoleColor.Green;
-    Console.ForegroundColor = ConsoleColor.Black;
+    Console.BackgroundColor = ConsoleColor.Gray;
+    Console.ForegroundColor = ConsoleColor.Magenta;
 
-    Console.WriteLine("Your color is green!");
+    Console.WriteLine("Your color is (on the gray background)!");
 }
-      else
+
+        else if (color == "yellow")
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Your color is yellow (on the black background)!");
+        }
+
+        else
 {
-    Console.BackgroundColor = ConsoleColor.Cyan;
+    Console.BackgroundColor = ConsoleColor.DarkRed;
     Console.ForegroundColor = ConsoleColor.Black;
 
-    Console.WriteLine("Your color is cyan!");
+    Console.WriteLine("Your color is else (on the Darkred background)!");
 }
   }
 }
