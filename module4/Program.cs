b90395f6ce23139d2b2b6727cfbe5a2c8ac1152f
ПКНЗ <@DESKTOP-OGI312N>
2024-12-14 21:42:49 +0300

    Цветастость

--- module4/Program.cs
+++ module4/Program.cs
@@ -3,48 +3,80 @@
 using System;
 using System.Drawing;
 
-namespace infinite_loop
+
+class MainClass
 {
-    class MainClass
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        Console.WriteLine("Напишите свой любимый цвет, но конечно же цветов ограниченный выбор, итак на выбор есть: \n\r красный,\n зеленый,\n голубой, \n серый,\n пурпурный,\n синий");
+
+        Console.WriteLine("цикл while");
+        int k = 7;
+
+        while (k > 1)
         {
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы\n red, green,cyan");
+            Console.WriteLine("попыток осталось: {0}", k);
+
+            k--;
 
             var color = Console.ReadLine();
             {
                 switch (color)
                 {
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is red!");
-                        break;
-
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is green!");
-                        break;
-
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is cyan!");
-                        break;
 
 
-                    default:
-                        Console.BackgroundColor = ConsoleColor.Yellow;
+                    case "красный":
+                        Console.BackgroundColor = ConsoleColor.Black;
                         Console.ForegroundColor = ConsoleColor.Red;
 
-                        Console.WriteLine("Your color is default!");
+                        Console.WriteLine("Ваш любимый цвет красный!");
+                        break;
+
+                    case "зеленый":
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                        Console.WriteLine("Ваш любимый цвет зеленый!");
+                        break;
+
+                    case "голубой":
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+
+                        Console.WriteLine("Ваш любимый цвет голубой!");
+                        break;
+
+                    case "пурпурный":
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+
+                        Console.WriteLine("Цвет шрифта пурпурный!");
+                        break;
+
+                    case "серый":
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                        Console.WriteLine("Ваш любимый цвет серый!");
+                        break;
+
+                    case "синий":
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+
+                        Console.WriteLine("цвет синенький");
+                        break;
+
+                   
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Вы не выбрали цвет из списка!");
                         break;
                 }
             }
         }
     }
 }
+
